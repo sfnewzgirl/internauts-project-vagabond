59289ce7235d6f7ecfab427f186a1e5d73825d2f
mrogach2350 <m.rogach@gmail.com>
2016-10-06 16:32:53 -0700

    addds email and password to migration

--- internauts-project-vagabond/db/migrate/20161006231334_create_users.rb
+++ internauts-project-vagabond/db/migrate/20161006231334_create_users.rb
@@ -4,6 +4,8 @@ class CreateUsers < ActiveRecord::Migration[5.0]
       t.string, :name
       t.string :current_city
       t.datetime :join_date
+      t.string :email
+      t.string :password_digest
 
       t.timestamps
     end
