a2ed3f7bb641121cf3a5129d70366a4d713baacc
Zachary Fong <fong.zachary@gmail.com>
2016-10-06 16:26:53 -0700

    removes users#index route

--- internauts-project-vagabond/config/routes.rb
+++ internauts-project-vagabond/config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root to: 'welcome#index'
 
-  get '/users', to: 'users#index', as: 'users'
   get '/users/new', to: 'users#new', as: 'new_user'
   post '/users', to: 'users#create'
   get '/users/:id', to: 'users#show', as: 'user'
