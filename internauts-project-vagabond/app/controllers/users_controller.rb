f12fbc88008a4aadf506b1a42804ae845656a655
mrogach2350 <m.rogach@gmail.com>
2016-10-06 17:20:08 -0700

    adds show method and updates create method

--- internauts-project-vagabond/app/controllers/users_controller.rb
+++ internauts-project-vagabond/app/controllers/users_controller.rb
@@ -1,12 +1,19 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find_by_id[:id]
+    @user = User.find_by_id(params[:id])
   end
   def new
     @user = User.new
   end
   def create
     user = User.new(user_params)
+
+    if user.save
+      redirect_to user_path(user.id)
+    else
+      flash[:sign_up_error] = "Something went wrong, please try again"
+      redirect_to new_user_path
+    end
   end
   def edit
   end
