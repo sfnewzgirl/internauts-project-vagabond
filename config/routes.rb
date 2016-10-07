f766ea396987e80a77c0bd254366e1b0ee2d672c
sfnewzgirl <YOUR_GITHUB_EMAIL_ADDRESS>
2016-10-07 16:47:34 -0700

    sprint 2 routes

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,12 @@ Rails.application.routes.draw do
   get '/logout', to: 'sessions#destroy'
 
   get '/posts/:id', to: 'posts#show', as: 'post'
+  get '/posts/new', to: 'posts#new', as: 'new_post'
+  post '/posts', to: 'posts#create'
+  get '/posts/:id/edit', to: 'posts#edit', as: 'edit_post'
+  patch '/posts/:id', to: 'posts#update'
+  delete '/posts/:id', to: 'posts#destroy'
+
+  get '/cities/:id', to: 'cities#show' as: 'city'
 
 end
