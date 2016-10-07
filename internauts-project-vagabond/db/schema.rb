a11e1113f21837f24b84fe685a3271c406168643
Zachary Fong <fong.zachary@gmail.com>
2016-10-06 17:34:10 -0700

    removes comma

--- internauts-project-vagabond/db/schema.rb
+++ internauts-project-vagabond/db/schema.rb
@@ -23,4 +23,14 @@ ActiveRecord::Schema.define(version: 20161006233813) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "current_city"
+    t.datetime "join_date"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
 end
