64363dbcc4e498bdef12ee6658e36becd94746b1
Zachary Fong <fong.zachary@gmail.com>
2016-10-06 18:25:39 -0700

    adds end

--- internauts-project-vagabond/db/schema.rb
+++ internauts-project-vagabond/db/schema.rb
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20161006233813) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+  end
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.string   "author"
