de91bb5daeeab35301c87a3ea520de5656df0379
mrogach2350 <m.rogach@gmail.com>
2016-10-06 17:50:08 -0700

    adds edit view

--- internauts-project-vagabond/db/schema.rb
+++ internauts-project-vagabond/db/schema.rb
@@ -12,7 +12,16 @@
 
 ActiveRecord::Schema.define(version: 20161006233813) do
 
+  # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.string   "author"
+    t.string   "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -22,12 +31,6 @@ ActiveRecord::Schema.define(version: 20161006233813) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
-  create_table "posts", force: :cascade do |t|
-    t.string   "title"
-    t.string   "author"
-    t.string   "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
 end
