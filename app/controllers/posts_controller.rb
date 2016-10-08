811f4f0ab19410bae55ddca37980e6a79120d2cc
sfnewzgirl <YOUR_GITHUB_EMAIL_ADDRESS>
2016-10-08 14:11:12 -0700

    changes route/id for city show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,27 @@
 class PostsController < ApplicationController
   def show
-    @post = Post.find_by_id(params[:id])
+    @post = Post.find_by_id(params[:post_id])
   end
   def new
     @post = Post.new
-    @city = City.find_by_id(params[:id])
+    @city = City.find_by_id(params[:city_id])
+  end
+  def create
+    city = City.find_by_id(params[:city_id])
+    new_post = Post.new
+    if new_post.save
+      city.posts << new_post
+      redirect_to post_path
+    else
+      redirect_to root_path
+    end
+    #save user_id
+    #save city_id
+    #need to turn author into logged in user
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :author, :content)
   end
 end
-
-
-# t.integer  "user_id"
-# t.integer  "city_id"
