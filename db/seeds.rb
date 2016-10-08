34885dfa2e016802cfa452bf6f39bbc38f9de878
Sera Bak <serabak15@gmail.com>
2016-10-07 22:56:41 -0700

    updates seed data and adds city images

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,45 +1,98 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-
-
+User.destroy_all
 Post.destroy_all
+City.destroy_all
 
-3.times do
-User.create({
-	name: FFaker::Name.name,
-	current_city: 'SF',
+users = User.create([
+	{
+	name: 'Misha LeClair',
+	current_city: 'San Francisco',
 	join_date: nil,
-	email: FFaker::Internet.email,
+	email: 'misha@ga.com',
 	password: '1234'
-	})
-end
+	},
+	{
+	name: 'Zach Fong',
+	current_city: 'San Francisco',
+	join_date: nil,
+	email: 'zach@ga.com',
+	password: '1234'
+	},
+	{
+	name: 'Mikey Rogachevsky',
+	current_city: 'San Francisco',
+	join_date: nil,
+	email: 'mikey@ga.com',
+	password: '1234'
+	},
+	{
+	name: 'Sera Bak',
+	current_city: 'San Francisco',
+	join_date: nil,
+	email: 'sera@ga.com',
+	password: '1234'
+	}
 
-
-3.times do
-Post.create({title: "Some Title",
-  author: FFaker::Name.name,
-  content: FFaker::HipsterIpsum.paragraph,
-  user_id: 1})
-end
-
+])
 
 City.create({
 	name: 'San Francisco',
-	photo: 'default.jpeg'
-	})
+	photo: 'SF.jpeg'
+		})
 
 City.create({
 	name: 'London',
-	photo: 'default.jpeg'
+	photo: 'london.jpeg'
 	})
 
 City.create({
 	name: 'Gibralter',
-	photo: 'default.jpeg'
+	photo: 'gibralter.jpeg'
 	})
+
+x = 1
+3.times do	
+	Post.create({
+		title: "Some Title" + x.to_s,
+	  	author: 'Misha LeClair',
+	  	content: FFaker::HipsterIpsum.paragraph,
+	  	user_id: 1,
+	  	city_id: x
+	  	})
+	x += 1
+end	
+
+y = 1
+3.times do	
+	Post.create({
+		title: 'Some Title' + y.to_s,
+	  	author: 'Zach Fong',
+	  	content: FFaker::HipsterIpsum.paragraph,
+	  	user_id: 2,
+	  	city_id: y
+	  	})
+	y += 1
+end	
+
+z = 1
+3.times do	
+	Post.create({
+		title: 'Some Title' + z.to_s,
+	  	author: 'Mikey Rogachevsky',
+	  	content: FFaker::HipsterIpsum.paragraph,
+	  	user_id: 3,
+	  	city_id: z
+	  	})
+	z += 1
+end	
+
+a = 1
+3.times do	
+	Post.create({
+		title: 'Some Title' + a.to_s,
+	  	author: 'Mikey Rogachevsky',
+	  	content: FFaker::HipsterIpsum.paragraph,
+	  	user_id: 4,
+	  	city_id: a
+	  	})
+	z += 1
+end	
