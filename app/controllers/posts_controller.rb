72ad305380c301702ed308d61840e5035379084d
sfnewzgirl <YOUR_GITHUB_EMAIL_ADDRESS>
2016-10-08 10:26:19 -0700

    returns new form route, controller, view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,13 @@
 class PostsController < ApplicationController
   def show
-    @post = Post.find(params[:id])
+    @post = Post.find_by_id(params[:id])
+  end
+  def new
+    @post = Post.new
+    @city = City.find_by_id(params[:id])
   end
 end
+
+
+# t.integer  "user_id"
+# t.integer  "city_id"
--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   get '/logout', to: 'sessions#destroy'
 
   get '/posts/:id', to: 'posts#show', as: 'post'
-  get '/posts/new', to: 'posts#new', as: 'new_post'
+  get '/cities/:id/posts/new', to: 'posts#new', as: 'new_post'
   post '/posts', to: 'posts#create'
   get '/posts/:id/edit', to: 'posts#edit', as: 'edit_post'
   patch '/posts/:id', to: 'posts#update'
