dc6d9b28caa163a2264fb5a0ff438e78280b649a
sfnewzgirl <YOUR_GITHUB_EMAIL_ADDRESS>
2016-10-09 15:18:37 -0700

    changes new post form to get city_id through

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,38 +8,22 @@ class PostsController < ApplicationController
     city_id = params[:city_id]
     @city = City.find_by(id: city_id)
   end
+
   def create
-    city = City.find_by_id(params[:city_id])
+    city = City.find(params[:city_id])
+    puts "got a city"
     new_post = Post.new(post_params)
+    puts "new_post.title = " + new_post.title
     if new_post.save
       city.posts << new_post
-      redirect_to post_path
+      redirect_to city_path
     else
       redirect_to root_path
     end
   end
 
-  #
-  # def create
-  #   city_id = params[:city_id]
-  #   @city = City.find_by(id: city_id)
-  #   # @city = City.find_by_id(params[:city_id])
-  #   new_post = Post.new(post_params)
-  #   if new_post.save
-  #     @city.posts << new_post
-  #     redirect_to city_path
-  #   else
-  #     redirect_to post_path
-  #   end
-  # end
-
-    #save user_id
-    #save city_id
-    #need to turn author into logged in user
-  # end
-
   private
   def post_params
-    params.require(:post).permit(:title, :author, :content, :city_id)
+    params.require(:post).permit(:title, :author, :content)
   end
 end
