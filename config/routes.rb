cbd84549ba56774ed201af2ce33a00bc85b7d48b
sfnewzgirl <YOUR_GITHUB_EMAIL_ADDRESS>
2016-10-09 11:38:05 -0700

    resources routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,31 +1,13 @@
 Rails.application.routes.draw do
-  root to: 'welcome#index'
 
-  get '/users/new', to: 'users#new', as: 'new_user'
-  post '/users', to: 'users#create'
-  get '/users/:id', to: 'users#show', as: 'user'
-  get '/users/:id/edit', to: 'users#edit', as: 'edit_user'
-  patch '/users/:id', to: 'users#update'
+  root "welcome#index"
 
+  resources :users do
+    resources :posts
+  end
 
-  get '/login', to: 'sessions#new'
-  post '/sessions', to: 'sessions#create'
-  get '/logout', to: 'sessions#destroy'
-
-  # misha tried this but it didn't work either
-  # resources :cities do
-  # resources :posts
-  # end
-
-  get '/posts/:post_id', to: 'posts#show', as: 'post'
-  get '/cities/:city_id/posts/new', to: 'posts#new', as: 'new_post'
-  post '/cities/:city_id/posts', to: 'posts#create'
-
-
-  get '/posts/:post_id/edit', to: 'posts#edit', as: 'edit_post'
-  patch '/posts/:post_id', to: 'posts#update'
-  delete '/posts/:post_id', to: 'posts#destroy'
-
-  get '/cities/:city_id', to: 'cities#show', as: 'city'
+  resources :cities do
+    resources :posts
+  end
 
 end
