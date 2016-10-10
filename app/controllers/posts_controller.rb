ca94f378f9206f31bc1abc35b80387a965bdffa2
sfnewzgirl <YOUR_GITHUB_EMAIL_ADDRESS>
2016-10-09 17:28:40 -0700

    saves a post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,22 +10,28 @@ class PostsController < ApplicationController
   end
 
   def create
-    current_user = User.find(params[:user_id])
-    city = City.find(params[:city_id])
-    puts "got a city"
-    new_post = Post.new(post_params)
-    puts "new_post.title = " + new_post.title
-    if new_post.save
-      city.posts << new_post
-      user.posts << new_post
-      redirect_to city_path
-    else
-      redirect_to root_path
-    end
+     @post = Post.create(post_params)
+     redirect_to city_path
   end
+
+  # def create
+  #   user = session[:user_ id]
+  #   city = City.find(params[:city_id])
+  #   puts "got a city"
+  #   new_post = Post.new(post_params)
+  #   puts "new_post.title = " + new_post.title
+  #   if new_post.save
+  #     city.posts << new_post
+  #     user.posts << new_post
+  #     redirect_to city_path
+  #   else
+  #     redirect_to root_path
+  #   end
+  # end
 
   private
   def post_params
-    params.require(:post).permit(:title, :author, :content)
+    post_info = params.require(:post).permit(:title, :author, :content)
+    post_params = post_info.merge({city_id: params[:city_id], user_id: current_user.id})
   end
 end
