525c70dc66c1b1f3c8475125f76fd7464e22a43f
sfnewzgirl <YOUR_GITHUB_EMAIL_ADDRESS>
2016-10-10 14:35:41 -0700

    edit/update route and posts controller methods and edit view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,6 +14,9 @@ class PostsController < ApplicationController
      @post = Post.create(post_params)
      @post.user = current_user
      redirect_to city_path
+
+# author: current_user.name,
+
   end
 
   def edit
@@ -28,13 +31,13 @@ class PostsController < ApplicationController
     post = Post.find_by(id: post_id)
     city_id = params[:city_id]
     city = City.find_by(id: city_id)
-    post.update_attributes(post_params)
-    redirect_to city_path
+    post.update(post_params)
+    redirect_to city_path(city)
   end
 
   private
   def post_params
     post_info = params.require(:post).permit(:title, :content)
-    post_params = post_info.merge({author: current_user.name, city_id: params[:city_id], user_id: current_user.id})
+    post_params = post_info.merge({city_id: params[:city_id], user_id: current_user.id})
   end
 end
