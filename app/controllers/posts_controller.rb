cbb5b9e2f246470211bc6c692823a1ca52a8c006
Sera Bak <serabak15@gmail.com>
2016-10-10 11:57:32 -0700

    fixes conficts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,24 @@
 class PostsController < ApplicationController
   def show
-    @post = Post.find(params[:id])
+    @post = Post.find_by_id(params[:post_id])
+    @city = City.find_by(id: @post.city_id)
+    @user = User.find_by(id: @post.user_id)
+  end
+
+  def new
+    @post = Post.new
+    city_id = params[:city_id]
+    @city = City.find_by(id: city_id)
+  end
+
+  def create
+     @post = Post.create(post_params)
+     redirect_to city_path
+  end
+
+  private
+  def post_params
+    post_info = params.require(:post).permit(:title, :author, :content)
+    post_params = post_info.merge({city_id: params[:city_id], user_id: current_user.id})
   end
 end
