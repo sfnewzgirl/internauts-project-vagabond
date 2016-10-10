33ab8dc01d71b1ebfb766ce7d48050a6f2611dea
sfnewzgirl <YOUR_GITHUB_EMAIL_ADDRESS>
2016-10-10 11:22:29 -0700

    new post form fills author as user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,27 +12,13 @@ class PostsController < ApplicationController
 
   def create
      @post = Post.create(post_params)
+     @post.user = current_user
      redirect_to city_path
   end
 
-  # def create
-  #   user = session[:user_ id]
-  #   city = City.find(params[:city_id])
-  #   puts "got a city"
-  #   new_post = Post.new(post_params)
-  #   puts "new_post.title = " + new_post.title
-  #   if new_post.save
-  #     city.posts << new_post
-  #     user.posts << new_post
-  #     redirect_to city_path
-  #   else
-  #     redirect_to root_path
-  #   end
-  # end
-
   private
   def post_params
-    post_info = params.require(:post).permit(:title, :author, :content)
-    post_params = post_info.merge({city_id: params[:city_id], user_id: current_user.id})
+    post_info = params.require(:post).permit(:title, :content)
+    post_params = post_info.merge({author: current_user.name, city_id: params[:city_id], user_id: current_user.id})
   end
 end
