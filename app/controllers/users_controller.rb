4b780dbc285e540093377e1c8bad9b3d562ca9d1
Zachary Fong <fong.zachary@gmail.com>
2016-10-11 11:36:23 -0700

    adds save validation to database to new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,14 +10,13 @@ class UsersController < ApplicationController
     @user = User.new
   end
   def create
-    user = User.new(user_params)
+    @user = User.new(user_params)
 
-    if user.save
-      login(user)
-      redirect_to user_path(user.id)
+    if @user.save
+      login(@user)
+      redirect_to user_path(@user.id)
     else
-      flash[:sign_up_error] = "That email has already been taken."
-      redirect_to new_user_path
+      render 'new'
     end
   end
   def edit
