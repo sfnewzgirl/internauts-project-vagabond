8c3ef53a6a47596758d04a86ee1be648b29e474d
Sera Bak <serabak15@gmail.com>
2016-11-17 14:32:43 -0800

    pulls in origin master

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -53,9 +53,9 @@ class PostsController < ApplicationController
     post_id = params[:id]
     post = Post.find_by(id: post_id)
     city_id = params[:city_id]
-    city = City.find_by(id: city_id)
+    # city = City.find_by(id: city_id)
     post.update(post_params)
-    redirect_to city_path(city)
+    redirect_to city_path(city_id)
   end
 
   def destroy
@@ -73,6 +73,6 @@ class PostsController < ApplicationController
   private
   def post_params
     post_info = params.require(:post).permit(:title, :content)
-    post_params = post_info.merge({city_id: params[:city_id], user_id: current_user.id, author: current_user.name})
+    post_info.merge({city_id: params[:city_id], user_id: current_user.id, author: current_user.name})
   end
 end
